443a02d662d1321c59f4f0298722bd37dcee9cec
Chien-Te Lee <Dexter.ChienTeLee@gmail.com>
2018-11-30 13:26:36 -0800

    reduce time of creating new NNs

--- defense_function.py
+++ defense_function.py
@@ -10,14 +10,12 @@ from art.utils import preprocess
 from create_Neural_Network import create_Neural_Network
 from evaluate import evaluate
 
-def defense_SpatialSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
-    # Use spatial smoothing defense
+def def_SpatialSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
     # reshape to smooth
     x_train = x_train.reshape(60000, 28, 28, 1)
     x_test = x_test.reshape(10000, 28, 28, 1)
     x_train_adv = x_train_adv.reshape(60000, 28, 28, 1)
     x_test_adv = x_test_adv.reshape(10000, 28, 28, 1)
-    
     # smooth
     smoother = SpatialSmoothing()
     x_train_smooth = smoother(x_train, window_size=3)
@@ -35,17 +33,16 @@ def defense_SpatialSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_te
     classifier.fit(x_train_smooth, y_train, nb_epochs=5, batch_size=50)
     
     # print result
-    print("After Defense\n")
+    print("After SpatialSmoothing Defense\n")
     evaluate(x_train_smooth, x_test_smooth, y_train, y_test, x_train_adv_smooth, x_test_adv_smooth, classifier)
     
     
-def defense_AdversarialTraining(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
-    ## Do adversarial training
-    # Data augmentation: expand the training set with the adversarial samples
+def def_AdversarialTraining(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
+    # expand the training set with the adversarial samples
     x_train_aug = np.append(x_train, x_train_adv, axis=0)
     y_train_aug = np.append(y_train, y_train, axis=0)
     
-    # Retrain the Network on the extended dataset
+    # retrain the Network on the extended dataset
     classifier = create_Neural_Network(min_, max_)
     classifier.fit(x_train_aug, y_train_aug, nb_epochs=5, batch_size=50)
     
@@ -54,8 +51,7 @@ def defense_AdversarialTraining(x_train, x_test, y_train, y_test, x_train_adv, x
     evaluate(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, classifier)
     
     
-def defense_FeatureSqueezing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
-    # Use FeatureSqueezing defense
+def def_FeatureSqueezing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
     squeezer = FeatureSqueezing()
     x_train_squeeze = squeezer(x_train, bit_depth=2)
     x_test_squeeze = squeezer(x_test, bit_depth=2)
@@ -71,8 +67,7 @@ def defense_FeatureSqueezing(x_train, x_test, y_train, y_test, x_train_adv, x_te
     evaluate(x_train_squeeze, x_test_squeeze, y_train, y_test, x_train_adv_squeeze, x_test_adv_squeeze, classifier)
     
     
-def defense_GaussianAugmentation(x_raw, x_raw_test, y_raw, y_raw_test, x_train_adv, x_test_adv, y_train, y_test, min_, max_):
-    ## Gaussian Agumentation
+def def_GaussianAugmentation(x_raw, x_raw_test, y_raw, y_raw_test, x_train_adv, x_test_adv, y_train, y_test, min_, max_):
     ga = GaussianAugmentation(sigma=150)
     x_train_aug, y_train_aug = ga(x_raw, y_raw)
     x_test_aug, y_test_aug = ga(x_raw_test, y_raw_test)
@@ -100,8 +95,7 @@ def defense_GaussianAugmentation(x_raw, x_raw_test, y_raw, y_raw_test, x_train_a
     acc = np.sum(preds == np.argmax(y_test, axis=1)) / y_test.shape[0]
     print('TEST-ADVERSARY: %.2f%% \n' % (acc * 100))
     
-def defense_LabelSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
-    ## LabelSmoothing
+def def_LabelSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_):
     labelsmoother = LabelSmoothing()
     x_train, y_train_smooth = labelsmoother(x_train, y_train, max_value=.8)
     x_test, y_test_smooth = labelsmoother(x_test, y_test, max_value=.8)
--- main.py
+++ main.py
@@ -3,14 +3,18 @@
 main
 """
 
+# import libraries
 from __future__ import absolute_import, division, print_function, unicode_literals
 from os.path import abspath
 import sys
 sys.path.append(abspath('.'))
 
+import numpy as np
 from art.utils import load_mnist_raw, preprocess
-from defense_function import defense_SpatialSmoothing, defense_AdversarialTraining, defense_FeatureSqueezing, defense_GaussianAugmentation, defense_LabelSmoothing
-from attack_function import attack_DeepFool, attack_BasicIterativeMethod, attack_FastGradientMethod, attack_JSMA, attack_CarliniL2Method
+
+from create_Neural_Network import create_Neural_Network
+from defense_function import def_SpatialSmoothing, def_AdversarialTraining, def_FeatureSqueezing, def_GaussianAugmentation, def_LabelSmoothing
+from attack_function import atk_DeepFool, atk_BasicIterative, atk_FastGradient, atk_JSMA, atk_CarliniAttack
 
 
 ## Read MNIST dataset (x_raw contains the original images):
@@ -21,26 +25,44 @@ x_train = x_train.reshape(60000, 784)
 x_test = x_test.reshape(10000, 784)
 
 
-# DeepFool + SpatialSmoothing
-x_test_adv, x_train_adv = attack_DeepFool(x_train, x_test, y_train, y_test, min_, max_)
-defense_SpatialSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_)
-
-# BasicIterativeMethod + AdversarialTraining
-x_test_adv, x_train_adv = attack_BasicIterativeMethod(x_train, x_test, y_train, y_test, min_, max_)
-defense_AdversarialTraining(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_)
-
-# FastGradientMethod + FeatureSqueezing
-x_test_adv, x_train_adv = attack_FastGradientMethod(x_train, x_test, y_train, y_test, min_, max_)
-defense_FeatureSqueezing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_)
+# create and train baseline netowrk
+classifier = create_Neural_Network(min_, max_)
+classifier.fit(x_train, y_train, nb_epochs=5, batch_size=50)
 
 
-# JSMA + GaussianAugmentation
-x_test_adv, x_train_adv = attack_JSMA(x_train, x_test, y_train, y_test, min_, max_)
-defense_GaussianAugmentation(x_raw, x_raw_test, y_raw, y_raw_test, x_train_adv, x_test_adv, y_train, y_test, min_, max_)
+# create attacks
+x_test_adv, x_train_adv = atk_DeepFool(x_train, x_test, y_train, y_test, classifier)
+x_train_adv_all = x_train_adv
+x_test_adv_all = x_test_adv
+
+x_test_adv, x_train_adv = atk_BasicIterative(x_train, x_test, y_train, y_test, classifier)
+x_train_adv_all = np.append(x_train_adv_all, x_train_adv, axis=0)
+x_test_adv_all = np.append(x_test_adv_all, x_test_adv, axis=0)
+
+x_test_adv, x_train_adv = atk_FastGradient(x_train, x_test, y_train, y_test, classifier)
+x_train_adv_all = np.append(x_train_adv_all, x_train_adv, axis=0)
+x_test_adv_all = np.append(x_test_adv_all, x_test_adv, axis=0)
 
 
-# CarliniL2Method + LabelSmoothing
-x_test_adv, x_train_adv = attack_CarliniL2Method(x_train, x_test, y_train, y_test, min_, max_)
-defense_LabelSmoothing(x_train, x_test, y_train, y_test, x_train_adv, x_test_adv, min_, max_)
+x_test_adv, x_train_adv = atk_JSMA(x_train, x_test, y_train, y_test, classifier)
+x_train_adv_all = np.append(x_train_adv_all, x_train_adv, axis=0)
+x_test_adv_all = np.append(x_test_adv_all, x_test_adv, axis=0)
+
+x_test_adv, x_train_adv = atk_CarliniAttack(x_train, x_test, y_train, y_test, classifier)
+x_train_adv_all = np.append(x_train_adv_all, x_train_adv, axis=0)
+x_test_adv_all = np.append(x_test_adv_all, x_test_adv, axis=0)
 
 
+print("============ Finish Creating Attacks ============")
+
+train_num = 60000
+test_num = 10000
+# utilize defenses
+def_SpatialSmoothing(x_train, x_test, y_train, y_test, x_train_adv_all[:train_num, :], x_test_adv_all[:test_num, :], min_, max_)
+def_AdversarialTraining(x_train, x_test, y_train, y_test, x_train_adv_all[train_num:2*train_num, :], x_test_adv_all[test_num:2*test_num, :], min_, max_)
+def_FeatureSqueezing(x_train, x_test, y_train, y_test, x_train_adv_all[2*train_num:3*train_num, :], x_test_adv_all[2*test_num:3*test_num, :], min_, max_)
+def_GaussianAugmentation(x_raw, x_raw_test, y_raw, y_raw_test, x_train_adv_all[3*train_num:4*train_num, :], x_test_adv_all[3*test_num:4*test_num, :], y_train, y_test, min_, max_)
+def_LabelSmoothing(x_train, x_test, y_train, y_test, x_train_adv_all[4*train_num:5*train_num, :], x_test_adv_all[4*test_num:5*test_num, :], min_, max_)
+
+
+print("============ Finish Utilizing Defenses ============")
